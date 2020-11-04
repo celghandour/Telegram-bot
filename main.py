faf0b4c728e5db36aa6a8213958296f984bd0389
Chafik <elghandour.chafik@gmail.com>
2020-11-04 13:53:18 +0100

    make it better

---
+++ main.py
@@ -0,0 +1,24 @@
+
+#Echo
+
+from telegram.ext import Updater, MessageHandler, Filters
+
+updater = Updater(token="1253254814:AAFlzuRtLylHykSZ0kK0LGFm9TDjoWJ164g")
+dispatcher = updater.dispatcher
+
+
+def echo(bot, update):
+
+    if "hello" in update.message.text:
+     bot.send_message(chat_id=update.message.chat_id,
+                     text="Hello,how are you baby ?!")
+    elif "love" in update.message.text:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="I mooooouuuuuuut 3likkkkk my Sghikiwta")
+    else:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="nti Basslaaaaa ?! ")
+
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+updater.start_polling()
